2a54ab12cef14b3ee36d5a4514294d8cbe7efcfc
binkybear <binkybear@nethunter.com>
2017-04-20 21:41:34 -0500

    Remove init.nethunter.rc
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -55,14 +55,6 @@ LOCAL_SRC_FILES := etc/ueventd.flo.rc
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := init.nethunter.rc
-LOCAL_MODULE_TAGS := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := etc/init.nethunter.rc
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
 # Bluetooth/Wifi config scripts
 
 include $(CLEAR_VARS)
